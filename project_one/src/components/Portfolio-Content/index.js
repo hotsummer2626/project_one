8c28c8197bd34c5eb06b874b38a5ac6536ab804c
SUNNY <hotsummer2626@gmail.com>
2021-06-16 23:54:06 +1000

    update project one dir

--- project_one/src/components/Portfolio-Content/index.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import "./index.css";
-
-export default class PortfolioContent extends Component {
-  render() {
-    return <div className="portfolio__content__info"></div>;
-  }
-}
